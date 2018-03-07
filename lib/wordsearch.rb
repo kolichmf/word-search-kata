520a3fde95279e56c91cc293472424c67a22ae98
kolichmf <mikekolich@gmail.com>
2018-03-06 21:37:42 -0500

    Wordsearch creates a hash representation of the puzzle

--- lib/wordsearch.rb
+++ lib/wordsearch.rb
@@ -1,7 +1,15 @@
 class Wordsearch
-  attr_reader :file, :search_words
+  attr_reader :file, :search_words, :puzzle
+
   def initialize(file)
     @file = file
-    @search_words = @file.readline.split(',')
+    @search_words = @file.readline.strip.split(',')
+    @puzzle = {}
+    @file.each_line.with_index do |line, y|
+      line.strip.split(',').each_with_index do |letter, x|
+        @puzzle[letter] ||= []
+        @puzzle[letter] << [x,y]
+      end
+    end
   end
 end
--- spec/classes/wordsearch_spec.rb
+++ spec/classes/wordsearch_spec.rb
@@ -6,11 +6,24 @@ describe Wordsearch do
     @wordsearch = Wordsearch.new(@file)
   end
 
-  it "should be take a file and make it attr_readable" do
-    expect(@wordsearch.file).to eq(@file)
-  end
+  describe "initialize" do
+    it "should be take a file and make it attr_readable" do
+      expect(@wordsearch.file).to eq(@file)
+    end
 
-  it "should take the comma separated words from the first line of the file and set them to an attr_readable array" do
-    expect(@wordsearch.search_words).to eq(File.foreach(@file).first.split(','))
+    it "should take the comma separated words from the first line of the file and set them to an attr_readable array" do
+      expect(@wordsearch.search_words).to eq(File.foreach(@file).first.strip.split(','))
+    end
+
+    it "should store the puzzle as a hash with each letter storing an array of coordinates where that letter appears" do
+      # rewind and remove the first line so we can test the puzzle portion
+      @file.rewind && @file.readline
+
+      @file.each_line.with_index do |line, y|
+        line.strip.split(',').each_with_index do |letter, x|
+          expect(@wordsearch.puzzle[letter].include? [x,y]).to be true
+        end
+      end
+    end
   end
 end
